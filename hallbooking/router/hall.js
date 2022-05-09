269cc2abaeb5ba443b04bc88ede99c92d6c93b70
BABYKALA MUTHU <92169527+Babykala@users.noreply.github.com>
2022-05-09 14:18:06 +0530

    Update hall.js

--- hallbooking/router/hall.js
+++ hallbooking/router/hall.js
@@ -3,13 +3,20 @@ const router=express.Router();
 
 const getModule=require('../module/hall');
 
-router.get('/get',getModule.getBook);
+//router.get('/get',getModule.getBook);
+
 router.post('/create',getModule.createBook);
+
 router.put('/update/:Id',getModule.updateBook);
+
 router.delete('/remove/:Id',getModule.deleteBook);
 
-//router.post('/createRoom',getModule.createRoom);
-//router.post('/createBookedRoom',getModule.createBookedRoom);
-//router.post('/createBookedCustomer',getModule.createBookedCustomer);
+router.post('/roomCreate',getModule.createRoom);
+
+router.post('/BookedData',getModule.createBookedData);
+
+router.get('/listOfBookedRooms',getModule.createBookedRoomList);
+
+//router.post('/customersBookingList',getModule.createBookedCustomer);
 
 module.exports = router;
